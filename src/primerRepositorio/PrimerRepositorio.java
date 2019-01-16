25c4e1b7da9a05fbf1d68f4898e836d7ba9c5dac
Andrea Muñoz Hidalgo <46522906+AndreaMH@users.noreply.github.com>
2019-01-16 10:51:14 +0100

    Update PrimerRepositorio.java

--- src/primerRepositorio/PrimerRepositorio.java
+++ src/primerRepositorio/PrimerRepositorio.java
@@ -5,7 +5,9 @@ public class PrimerRepositorio {
 	public static void main(String[] args) {
 		
 		System.out.println("Primer repositorio para GitHub");
-		System.out.println("\nCreado por Carlos Fern·ndez DÌez");
+		System.out.println("\nCreado por Carlos Fern√°ndez D√≠ez");
+		
+		System.out.println("SEXY BITCH");
 
 	}
 
